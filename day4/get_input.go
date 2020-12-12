ecad1a2bddd50c41186a07a72f2b7464936d459a
Bart Hogenes <barthogenes@gmail.com>
2020-12-12 12:38:38 +0100

    Splitting passport string into separate lines

--- day4/get_input.go
+++ day4/get_input.go
@@ -1,14 +1,31 @@
 package day4
 
 import (
+	"strings"
+
 	"github.com/barthogenes/adventofcode2020/api"
 )
 
 // GetInput Get the input for the fourth day.
 func GetInput(api api.AdventOfCodeAPI) []Passport {
-	return parseInput(api.GetInputForDay(4))
+	input := api.GetInputForDay(4)
+	return parseInput(input)
 }
 
 func parseInput(input string) []Passport {
+	passportStrings := splitIntoStrings(input)
+	return parsePassports(passportStrings)
+}
+
+func splitIntoStrings(input string) []string {
+	lines := strings.Split(input, "\n\n")
+	for index, line := range lines {
+		lines[index] = strings.ReplaceAll(line, "\n", " ")
+	}
+
+	return lines
+}
+
+func parsePassports([]string) []Passport {
 	return []Passport{}
 }
