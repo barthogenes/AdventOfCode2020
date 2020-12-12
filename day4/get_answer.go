f8783ab0d2c3825613a0b1f96b844aeb21bdf50a
Bart Hogenes <barthogenes@gmail.com>
2020-12-12 19:35:56 +0100

    Finish validating passports function for part 1

--- day4/get_answer.go
+++ day4/get_answer.go
@@ -2,5 +2,22 @@ package day4
 
 // GetAnswer Get the answer for the Day 4 puzzle.
 func GetAnswer(input []Passport) int {
-	return 0
+	validPassportsCount := 0
+	for _, passport := range input {
+		if isValid(passport) {
+			validPassportsCount++
+		}
+	}
+
+	return validPassportsCount
+}
+
+func isValid(passport Passport) bool {
+	return passport.BirthYear != 0 &&
+		passport.IssueYear != 0 &&
+		passport.ExpirationYear != 0 &&
+		passport.Height != "" &&
+		passport.HairColor != "" &&
+		passport.EyeColor != "" &&
+		passport.PassportID != ""
 }
