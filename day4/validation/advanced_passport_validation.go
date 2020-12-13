ef117751181900f65a1f8fed3e4dd44fd76b1ef4
Bart Hogenes <barthogenes@gmail.com>
2020-12-13 15:45:46 +0100

    Implementation of the passport validation

--- day4/validation/advanced_passport_validation.go
+++ day4/validation/advanced_passport_validation.go
@@ -1,5 +1,12 @@
 package validation
 
+import (
+	"regexp"
+	"strings"
+
+	"github.com/barthogenes/adventofcode2020/util"
+)
+
 // AdvancedPassportValidation ...
 func AdvancedPassportValidation(passport Passport) bool {
 	return isBetween(passport.BirthYear, 1920, 2002) &&
@@ -12,21 +19,61 @@ func AdvancedPassportValidation(passport Passport) bool {
 }
 
 func isBetween(value, min, max int) bool {
-	return false
+	return value >= min && value <= max
 }
 
 func isValidHeight(height string) bool {
+	getHeight := func(suffix string) int {
+		heightWithoutSuffix := strings.TrimSuffix(height, suffix)
+		if len(heightWithoutSuffix) == len(height) {
+			return 0
+		}
+		return util.ToNumber(heightWithoutSuffix)
+	}
+	heightInCm := getHeight("cm")
+	heightInInches := getHeight("in")
+	if heightInCm > 0 && heightInCm >= 150 && heightInCm <= 193 {
+		return true
+	}
+	if heightInInches > 0 && heightInInches >= 59 && heightInInches <= 76 {
+		return true
+	}
+
 	return false
 }
 
 func isValidColor(color string) bool {
-	return false
+	isValid, err := regexp.MatchString("^#[\\dabcdef]{6}$", color)
+	if err != nil {
+		panic(err)
+	}
+	return isValid
 }
 
 func isValidEyeColor(eyeColor string) bool {
+	switch eyeColor {
+	case "amb":
+		return true
+	case "blu":
+		return true
+	case "brn":
+		return true
+	case "gry":
+		return true
+	case "grn":
+		return true
+	case "hzl":
+		return true
+	case "oth":
+		return true
+	}
 	return false
 }
 
 func isValidPassportID(passportID string) bool {
-	return false
+	isValid, err := regexp.MatchString("^[\\d]{9}$", passportID)
+	if err != nil {
+		panic(err)
+	}
+	return isValid
 }
