062a69bd89051a0d1de265065ac80dd528b94fbd
Bart Hogenes <barthogenes@gmail.com>
2021-05-07 16:08:12 +0200

    Folder setup for day8

---
+++ day8/get_answer.go
@@ -0,0 +1,6 @@
+package day8
+
+// GetAnswerForPart1 Get the answer for part 1 of the Day 7 puzzle.
+func GetAnswerForPart1(input string) string {
+	return input
+}
---
+++ day8/get_input.go
@@ -0,0 +1,10 @@
+package day8
+
+import (
+	"github.com/barthogenes/adventofcode2020/api"
+)
+
+// GetInputForPart1 Get the input for the 8th day.
+func GetInputForPart1(api api.AdventOfCodeAPI) string {
+	return api.GetInputForDay(8)
+}
